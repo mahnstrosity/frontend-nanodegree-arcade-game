5849187476e4ab0ef9f7fe7237c9aedfdd86fa6a
mahnstrostiy <markmahn@verizon.net>
2016-02-03 17:20:48 -0500

    created enemies array and gameboard is now showing

--- js/app.js
+++ js/app.js
@@ -24,12 +24,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/enemy-bug.png';
+};
+Player.prototype.update = function(){
+
+};
+Player.prototype.render = function(){
+
+};
+Player.prototype.handleInput = function(){
+
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [new Enemy()];
 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
@@ -44,3 +58,5 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
